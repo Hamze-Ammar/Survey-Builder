a569776a3a9661974025d8ebf970e1d03989bdb9
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-06-17 15:39:09 +0300

    create: Question model, controller, table | Laravel

---
+++ Laravel backend/database/migrations/2022_06_17_113743_create_questions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('context');
+            $table->string('type');
+            $table->bigInteger('survey_id');
+            $table->longText('choices')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
