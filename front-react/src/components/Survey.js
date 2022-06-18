1bc4ca3020b53274a747e96099bfd6f11d2d738f
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-06-18 23:14:26 +0300

    Added Survey component | React

--- front-react/src/components/Survey.js
+++ front-react/src/components/Survey.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Survey(survey) {
-
+    let id = survey.survey.id;
+    // console.log(id);
+    let location =`/fill_survey/?id=${id}` ;
   return (
     <div className="Survey-box">
-      <h3>{survey.survey.name}</h3>
-      <p>{survey.survey.num_of_questions} questions</p>
-
+        <Link to={location} state={{id}}> <h3>{survey.survey.name}</h3>
+      <p>{survey.survey.num_of_questions} questions</p>    </Link>
+            
+    
     </div>
   );
 }
