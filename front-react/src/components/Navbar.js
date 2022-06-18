8c6ad4198e00ee1f9f0ca7a768e8495755b66957
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-06-18 18:46:23 +0300

    Added component: Navbar | React

--- front-react/src/components/Navbar.js
+++ front-react/src/components/Navbar.js
@@ -1,33 +1,31 @@
 import React from "react";
-import classes from "./Navbar.module.css";
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
     <div>
       <div className="topnav">
         <div>
-            <a className="active" href="">
-          Survey Builder
-        </a>
-        <a href="#">All Surveys</a>
-        <a href="#">MySurveys</a>
-        <a href="#">MyAnswers</a>
+          <a className="active" href="">
+            Survey Builder
+          </a>
+          <a href="#" onClick={props.allSurveys}>
+            All Surveys
+          </a>
+          <a href="#">MySurveys</a>
+          <a href="#">MyAnswers</a>
         </div>
-        <div className="topnav-middle">
-
-        </div>
+        <div className="topnav-middle"></div>
         <div>
-           <a className="topnav-right" href="#">
-          Login
-        </a>
-        <a className="topnav-right" href="#">
-          register
-        </a>
-        <a className="topnav-right" href="#">
-          Logout
-        </a> 
+          <a className="topnav-right" href="#">
+            Login
+          </a>
+          <a className="topnav-right" href="#">
+            register
+          </a>
+          <a className="topnav-right" href="#">
+            Logout
+          </a>
         </div>
-        
       </div>
     </div>
   );
