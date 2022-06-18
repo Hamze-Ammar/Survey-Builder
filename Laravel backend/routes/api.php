7975d0f065d12f5b3f5ab4497c333f8b748e52ee
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-06-18 08:11:46 +0300

    Added: prefixes routes | Laravel

--- Laravel backend/routes/api.php
+++ Laravel backend/routes/api.php
@@ -8,23 +8,29 @@ use App\Http\Controllers\SurveyController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\AnswerController;
 
+Route::group(['prefix' => 'v1'], function(){
+    Route::group(['prefix' => 'user'], function(){
+        Route::group(['middleware' => 'api'], function($router) {
+            Route::post('/register', [JWTController::class, 'register']);
+            Route::post('/login', [JWTController::class, 'login']);
+            Route::post('/logout', [JWTController::class, 'logout']);
+            Route::post('/refresh', [JWTController::class, 'refresh']);
+            Route::post('/profile', [JWTController::class, 'profile']);
+        });
+    });
 
+    Route::group(['prefix' => 'admin'], function(){
+        Route::group(['middleware' => 'role.admin'], function(){
+            Route::post('/add_survey', [SurveyController::class, 'addSurvey']);
+            Route::post('/add_question', [QuestionController::class, 'addQuestion']);
+        });
+    });
 
-Route::group(['middleware' => 'api'], function($router) {
-    Route::post('/register', [JWTController::class, 'register']);
-    Route::post('/login', [JWTController::class, 'login']);
-    Route::post('/logout', [JWTController::class, 'logout']);
-    Route::post('/refresh', [JWTController::class, 'refresh']);
-    Route::post('/profile', [JWTController::class, 'profile']);
+    Route::group(['prefix' => 'survey'], function(){
+        Route::post('/get_all_questions', [QuestionController::class, 'getAllQuestions']);
+        Route::get('/get_question_choices', [QuestionController::class, 'getQuestionWithChoices']);
+        Route::get('/get_servey_questions/{id}', [SurveyController::class, 'getServeyQuestions']);
+    });
+
+    Route::get('/not_found', [UserController::class, 'notFound'])->name("not-found");
 });
-
-//Those are for admin, a middleware to be added
-Route::post('/add_survey', [SurveyController::class, 'addSurvey']);
-Route::post('/add_question', [QuestionController::class, 'addQuestion']);
-
-//Testing eloquent
-Route::post('/get_all_questions', [QuestionController::class, 'getAllQuestions']);
-Route::get('/get_question_choices', [QuestionController::class, 'getQuestionWithChoices']);
-
-Route::get('/get_servey_questions/{id}', [SurveyController::class, 'getServeyQuestions']);
-
