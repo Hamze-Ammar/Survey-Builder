baf75b44a2c3156d46a9f5e852560444baa84ce3
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-06-20 22:32:32 +0300

    Added Text Component | React

--- front-react/src/components/Text.js
+++ front-react/src/components/Text.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 export default function Text(props) {
+  let submitQuestions = props.submitQuestions;
+  let question_id = props.question_id;
+  const [answer, setAnswer] = useState("");
+  // console.log(answer);
+  // console.log("from text line7 " + submitQuestions, question_id);
+
+  // Prepare to send data on submit:
+  useEffect(() => {
+    if (submitQuestions && question_id) {
+            submitAnswer(answer);
+        // Try redirect admin to a new page once the suyvey is submitted
+    }
+  }, [submitQuestions, question_id]);
+
+  const submitAnswer = async (mycontext) => {
+    let info = {};
+    info.url = "http://127.0.0.1:8000/api/v1/survey/save_answer";
+    info.token = "Bearer " + localStorage.getItem("access_token");
+    let context = mycontext;
+    info.data = { context, question_id };
+    sendRequest(info);
+  };
+
+  const sendRequest = async (info) => {
+    const res = await fetch(info.url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: info.token,
+      },
+      body: JSON.stringify(info.data),
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
     <div>
-        <input  type="text" placeholder='Type your answer here...'  style={{ backgroundColor: "white" }}/>
+      <input
+        onChange={(e) => {
+          setAnswer(e.target.value);
+        }}
+        type="text"
+        placeholder="Type your answer here..."
+        style={{ backgroundColor: "white" }}
+      />
     </div>
-  )
+  );
 }
