e7aa35c166d9040de7597caa7ad6d40636808477
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-06-20 15:46:48 +0300

    Added Component: Text | React

---
+++ front-react/src/components/Text.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Text(props) {
+  return (
+    <div>
+        <input  type="text" placeholder='Type your answer here...'  style={{ backgroundColor: "white" }}/>
+    </div>
+  )
+}
