6e764463b41e160d6990f3e5d43dcb7bc33d7fbf
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-06-20 15:47:28 +0300

    Added Component: Choices | React

--- front-react/src/components/Choices.js
+++ front-react/src/components/Choices.js
@@ -1,23 +1,30 @@
 import React from "react";
+import Checkbox from "./Checkbox";
 import RadioChoice from "./RadioChoice";
 
 export default function Choices(props) {
-    let choices = props.choices;
+  let choices = props.choices;
   //console.log(type.toString());
   // console.log(type)
-  console.log("from choices line 8: " + props.type);
-//   console.log("from choices line 9: " + props.choices.choices);
+  console.log("from choices line 8: " + props);
+  //   console.log("from choices line 9: " + props.choices.choices);
   return (
     <>
-    {props.type==="radio" && <>
-      <form>
-          {" "}
-          {choices.map((choice) => (
-            <RadioChoice key={choice.id} choice={choice} />
-          ))}{" "}
-        </form>
-    </>
-}
+      {props.type === "radio" && (
+        <>
+            {" "}
+            {choices.map((choice) => {
+              return <RadioChoice key={choice.id} choice={choice} />;
+            })}{" "}
+        </>
+      )}
+
+      {props.type === 'checkbox' && ( <>
+        {choices.map((choice) => {
+          return <Checkbox key={choice.id} choice={choice} />
+        })}
+        </>
+      )}
     </>
   );
 }
