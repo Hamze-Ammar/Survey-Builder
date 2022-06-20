30c3ddcc5055146e2ecd6d7acdbc5a3b4ceff3fb
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-06-20 22:32:46 +0300

    Added Range Component | React

--- front-react/src/components/Range.js
+++ front-react/src/components/Range.js
@@ -1,11 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-export default function Range({ choices }) {
+export default function Range({ choices, question_id, submitQuestions }) {
   //console.log(choices[0].context);
   let min = choices[0].context;
   let max = choices[1].context;
-  console.log(min);
-  const [rangeValue, setRangeValue] = useState("");
+  //console.log(min);
+  const [answer, setAnswer] = useState('');
+
+  // console.log("from Range line10 "+ submitQuestions,question_id)
+
+   // Prepare to send data on submit:
+   useEffect(() => {
+    if (submitQuestions && question_id) {
+            submitAnswer(answer);
+        // Try redirect admin to a new page once the suyvey is submitted
+    }
+  }, [submitQuestions, question_id]);
+
+  const submitAnswer = async (mycontext) => {
+    let info = {};
+    info.url = "http://127.0.0.1:8000/api/v1/survey/save_answer";
+    info.token = "Bearer " + localStorage.getItem("access_token");
+    let context = mycontext;
+    info.data = { context, question_id };
+    sendRequest(info);
+  };
+
+  const sendRequest = async (info) => {
+    const res = await fetch(info.url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: info.token,
+      },
+      body: JSON.stringify(info.data),
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+
 
   return (
     <div>
@@ -18,13 +51,13 @@ export default function Range({ choices }) {
           min={min}
           max={max}
           onChange={(e) => {
-            setRangeValue(e.target.value);
+            setAnswer(e.target.value);
           }}
         />
         {max} : max
       </label>{" "}
       <br /> <br /> <br />
-      {rangeValue && rangeValue}
+      {answer && answer}
     </div>
   );
 }
