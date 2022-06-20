cb4efad9453da68a9081c9cef5868e64f2fc7a68
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-06-20 22:34:50 +0300

    Updated: Questions Component | React

--- front-react/src/components/Questions.js
+++ front-react/src/components/Questions.js
@@ -1,15 +1,17 @@
 import React from "react";
 import Question from "./Question";
 
-export default function Questions(questions) {
-    //console.log(questions.questions);
-    questions = questions.questions;
-    //console.log(questions);
+export default function Questions(props) {
+  //console.log(questions.questions);
+  let questions = props.questions;
+  let submitQuestions = props.submitQuestions;
+  //console.log(questions);
+  //console.log("hi");
 
   return (
     <div>
       {questions.map((question, index) => (
-        <Question key={question.id} question={question} index={index} />
+        <Question key={question.id} question={question} index={index} submitQuestions={submitQuestions} />
       ))}
     </div>
   );
