50dc7ed50569bfecb647b4c4cad6e42bae8b4cdf
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-06-20 15:47:49 +0300

    Added Component: Number | React

---
+++ front-react/src/components/Number.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Number() {
+  return (
+    <div>
+        <input placeholder='Type Number' className='normal-height' type="number" name="" id="" />
+    </div>
+  )
+}
