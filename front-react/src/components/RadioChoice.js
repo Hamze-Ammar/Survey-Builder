4463b05b23ec1be54687709154e34d0643c2d649
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-06-20 22:33:00 +0300

    Added RadioChoice Component | React

--- front-react/src/components/RadioChoice.js
+++ front-react/src/components/RadioChoice.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function RadioChoice(props) {
   let choice = props.choice;
-  console.log("props", props);
-  console.log("props", props.choice.question_id);
+  let question_id = props.question_id;
+  // console.log("props.question_id: question_id", question_id);
+  // console.log("props", props.choice.question_id);
+  let submitQuestions = props.submitQuestions;
+  // console.log("from radioChoice line 8: " +question_id, submitQuestions);
+
+  
+
   return (
     <>
-      
       <input
         type="radio"
         id={choice.context}
         name={props.choice.question_id}
         value={choice.context}
-      /> {"  "}
+        onChange={(e)=>{props.onchange(e.target.value)}}
+      />{" "}
+      {"  "}
       <label htmlFor={choice.context}>{choice.context}</label>
       <br /> <br />
     </>
